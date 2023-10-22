0278bf84c998ad903762213e0d2fde2315a047c5
Pitra Winarianto <123644468+ptr173@users.noreply.github.com>
2023-10-22 22:26:35 +0700

    Create src.js

---
+++ Forensic/BeeCTF/Black Market/src/src.js
@@ -0,0 +1,14 @@
+var prefix = 'BEEFEST';
+var A = 'M4l1Cio5';
+var B = 'Th1S';
+var D = 'PDF';
+var d = 'W1tH';
+var f = 'J4vAsCR1p7';
+var p = '15_3mbeD3d';
+
+flag = 'BEEFEST{F4k3_fl4g}';
+Real = prefix + '{' + B + '_' + A + '_' + D + '_' + p + '_' + d + '_' + f +'}';
+
+while (flag !== Real ){
+    app.alert("HAHA, YOUR COMPUTER HAS BEEN INFECTED BY THIS PDF");
+}
